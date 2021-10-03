de8b1d095a31f96242296a0851a507a2f8a0d9d5
VChet <17050347+VChet@users.noreply.github.com>
2021-10-04 01:27:45 +0500

    feat: add footer

--- src/App.svelte
+++ src/App.svelte
@@ -10,10 +10,39 @@
   <TrooperBlock {current} />
   <RosterBlock bind:current />
 </main>
+<footer class="footer">
+  <div>
+    All game resources and images belong to their respective owner —
+    <a href="https://inthekillhouse.com" rel="noopener">KillHouse Games</a>
+  </div>
+  <a
+    href="https://github.com/VChet/dk2-loadout/issues"
+    target="_blank"
+    rel="noopener"
+  >
+    Report Bug
+  </a>
+</footer>
 
 <style lang="scss">
   .container {
     display: flex;
     align-items: flex-start;
+  }
+  .footer {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 25%;
+    justify-content: space-around;
+    margin-top: 20px;
+    padding: 5px;
+    background-color: var(--bg-main);
+    a {
+      color: var(--accent);
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 </style>
