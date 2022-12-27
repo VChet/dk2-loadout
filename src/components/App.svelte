33b65dd7969bfca8b3dafc9c4f43dbfba801fb66
VChet <17050347+VChet@users.noreply.github.com>
2022-12-27 23:58:27 +0400

    chore: remove counter.dev

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import ManagerBlock from "@/components/Manager.svelte";
   import TrooperBlock from "@/components/Trooper.svelte";
   import RosterBlock from "@/components/Roster.svelte";
-  import { initAnalytics } from "@/utilities/counter.dev";
   import type { Roster, Trooper } from "@/types/Roster";
 
   let roster: Roster;
   let selectedTrooper: Trooper;
-
-  onMount(() => {
-    initAnalytics();
-  });
 </script>
 
 <main class="container">
